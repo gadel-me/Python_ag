5f3befba4737f40d3764e2a2e8a89997ea04177f
gadel-me <andreas.gadelmeier@gmx.net>
2019-05-17 00:47:46 +0200

    Fixed some bugs

--- PYMODULES/VMD/ag_vmd.py
+++ PYMODULES/VMD/ag_vmd.py
@@ -263,7 +263,7 @@ def vmd_render_scene(image_out, image_size=[2000, 2000], renderer="TachyonLOptiX
     VMD.evaltcl("light 0 on")
     image = Image.open("{}.ppm".format(image_out))
     image.save("{}.png".format(image_out), format="PNG")
-    display.set(size=disp_default_size)
+    #display.set(size=disp_default_size)
     display.update()
     display.update_ui()
     os.remove("{}.ppm".format(image_out))
