7db0a615b84bb9fd5179dbde8d9b613c1c0b1350
gadel-me <andreas.gadelmeier@gmx.net>
2019-07-31 19:47:28 +0200

    Bug fixing

--- PYSCRIPTS/LAMMPS/merge_lmpdats.py
+++ PYSCRIPTS/LAMMPS/merge_lmpdats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 import argparse
+import ag_lammps
 #import math
 #import md_box as mdb
-import ag_unify_md as agum
+#import ag_unify_md as agum
+
 
 # Argument Parsing -------------------------------------------------------------
 parser = argparse.ArgumentParser(
@@ -20,7 +22,15 @@ parser.add_argument("-lmpdats",
                     required=True,
                     default=None,
                     metavar="foo.lmpdat",
-                    help="Lammps data file. Provides topology and force field parameters."
+                    help="Lammps data files. Provides topology and force field parameters."
+                    )
+
+parser.add_argument("-dcds",
+                    nargs="*",
+                    required=True,
+                    default=None,
+                    metavar="foo.dcd",
+                    help="Lammps dcd files. Provides coordinates only."
                     )
 
 parser.add_argument("-sysname",
@@ -37,16 +47,8 @@ cell.add_argument("-cell",
                        "(in degrees) in that particular order."
                   )
 
-#cell.add_argument("-cbg",
-#                  action="store_true",
-#                  help="Guess cell by atomic coordinates."
-#                  )
-#
-#cell.add_argument("-cbc",
-#                  metavar="foo.lmpdat",
-#                  default=None,
-#                  help="Name of data file from which the coordinates should be taken."
-#                  )
+cell.add_argument("-show_lattice_cells", default=False, action="store_true")
+parser.add_argument("-delete_close_atoms", default=False, action="store_true", help="BUGGY! Atoms from the system that is added to the previous one will be deleted if in close contact with existing atoms.")
 
 parser.add_argument("-out",
                     metavar="foobar.lmpdat",
@@ -59,16 +61,75 @@ args = parser.parse_args()
 # Modeling ---------------------------------------------------------------------
 sys_all = []
 
-for lmpdat in args.lmpdats:
-    sys = agum.Unification()
-    sys.read_lmpdat(lmpdat, energy_unit="eV", angle_unit="deg")
-    sys_all.append(sys)
+for idx, lmpdat in enumerate(args.lmpdats):
 
+    # read dcds files if available
+    try:
+        curdcd = args.dcds[idx]
+    except IndexError:
+        curdcd = None
+
+    cursys = ag_lammps.read_lmpdat(lmpdat, dcd=curdcd, frame_idx_start=-1, frame_idx_stop=-1)
+    sys_all.append(cursys)
+
+    if args.show_lattice_cells is True:
+        cursys.ts_boxes[-1].box_lmp2lat()
+        print("System {}".format(idx))
+        print(cursys.ts_boxes[-1].ltc_a)
+        print(cursys.ts_boxes[-1].ltc_b)
+        print(cursys.ts_boxes[-1].ltc_c)
+        print(cursys.ts_boxes[-1].ltc_alpha)
+        print(cursys.ts_boxes[-1].ltc_beta)
+        print(cursys.ts_boxes[-1].ltc_gamma)
+        print("\n")
+
+if args.show_lattice_cells is True:
+    print("Exiting.")
+    exit()
+
+# extend first molecular system with all others
 for idx in xrange(1, len(sys_all)):
-    sys_all[0].extend_universe(sys_all[idx])
 
-sys_all[0].mix_pair_types(mode="ij")
-sys_all[0].fetch_molecules_by_bonds()
-sys_all[0].mols_to_grps()
+    if args.delete_close_atoms is True:
+        natoms = len(sys_all[idx - 1].atoms)
+
+    sys_all[0].extend_universe(sys_all[idx], u1_frame_id=-1, u2_frame_id=-1)
+    sys_all[0].refresh()
+    sys_all[0].fetch_molecules_by_bonds()
+    sys_all[0].mols_to_grps()
+
+    # delete atoms from the system that was added
+    if args.delete_close_atoms is True:
+        sys_all[0].create_linked_cells(-1)
+        close_atoms = sys_all[0].chk_atm_dist(min_dist=1.0)
+        delete_atoms = [i for i in close_atoms if i > natoms]
+        print(close_atoms)
+        print(delete_atoms)
+        sys_all[0].delete_atoms(*delete_atoms)
+        sys_all[0].refresh()
+        sys_all[0].fetch_molecules_by_bonds()
+        sys_all[0].mols_to_grps()
+
+#sys_all[0].mix_pair_types(mode="ij")
+sys_all[0].pair_types = []
+#sys_all[0].fetch_molecules_by_bonds()
+#sys_all[0].mols_to_grps()
 sys_all[0].change_indices(incr=1, mode="increase")
-sys_all[0].write_lmpdat(args.out, title=args.sysname, cgcmm=True)
+
+# convert box
+sys_all[0].ts_boxes[-1].box_lmp2lat()
+sys_all[0].ts_boxes[-1].ltc_a = args.cell[0]
+sys_all[0].ts_boxes[-1].ltc_b = args.cell[1]
+sys_all[0].ts_boxes[-1].ltc_c = args.cell[2]
+sys_all[0].ts_boxes[-1].ltc_alpha = args.cell[3]
+sys_all[0].ts_boxes[-1].ltc_beta = args.cell[4]
+sys_all[0].ts_boxes[-1].ltc_gamma = args.cell[5]
+sys_all[0].ts_boxes[-1].box_lat2lmp()
+
+# check interatomic distances
+sys_all[0].create_linked_cells(-1)
+close_atoms = sys_all[0].chk_atm_dist(min_dist=1.2)
+close_atoms = " ".join(map(str, close_atoms))
+print("***Warning: Atoms with close contacts found: \n" + close_atoms)
+
+sys_all[0].write_lmpdat(args.out, frame_id=-1, title=args.sysname, cgcmm=True)
--- PYTOOLS/LAMMPS/cut_box_from_box.py
+++ PYTOOLS/LAMMPS/cut_box_from_box.py
@@ -8,6 +8,9 @@ import ag_vectalg as agv
 import md_box as mdb
 import ag_lammps
 
+#TODO:  Not the expected behavior when shifting the box since the cut_shape
+#       function needs an option (like in a dict) to determine if the atoms above (positive distance)
+#       or below (negative distance) a plane should be neglected.
 
 """
 Cut a shape by its box vectors from a given lammps file. The shape or the negative
@@ -16,7 +19,7 @@ of it may be returned. Cell from lammps data file must be unwrapped.
 #===============================================================================
 # HELPER FUNCTIONS
 #===============================================================================
-def cutting(main_sys, cutting_box, inverse=True, frame_id=-1, shift=(0, 0, 0)):
+def cutting(main_sys, cutting_box, inverse=True, frame_id=-1, shift=None):
     """
     Cut the shape of cutting_box from main_sys.
 
@@ -38,16 +41,22 @@ def cutting(main_sys, cutting_box, inverse=True, frame_id=-1, shift=(0, 0, 0)):
     if cutting_box.boxtype == "lammps":
         cutting_box.box_lmp2cart()
 
-    shift = np.array(shift)
+    if shift is None:
+        shift = np.array([0, 0, 0])
+    elif not isinstance(shift, np.ndarray):
+        shift = np.array(shift)
+    else:
+        pass
 
     # define box faces
-    face_1 = agv.get_plane(cutting_box.crt_a, cutting_box.crt_b)  # xy
-    face_2 = agv.get_plane(cutting_box.crt_c, cutting_box.crt_a)  # xz
-    face_3 = agv.get_plane(cutting_box.crt_b, cutting_box.crt_c)  # yz
+    face_1 = agv.get_plane(cutting_box.crt_a, cutting_box.crt_b, vt_p=shift)  # xy
+    face_2 = agv.get_plane(cutting_box.crt_c, cutting_box.crt_a, vt_p=shift)  # xz
+    face_3 = agv.get_plane(cutting_box.crt_b, cutting_box.crt_c, vt_p=shift)  # yz
     # multiply with -1 because normal vector must show in the opposite direction
-    face_4 = [-1 * i for i in agv.get_plane(cutting_box.crt_a, cutting_box.crt_b, vt_p=agv.add_vts(cutting_box.crt_c, shift))]
-    face_5 = [-1 * i for i in agv.get_plane(cutting_box.crt_c, cutting_box.crt_a, vt_p=agv.add_vts(cutting_box.crt_b, shift))]
-    face_6 = [-1 * i for i in agv.get_plane(cutting_box.crt_b, cutting_box.crt_c, vt_p=agv.add_vts(cutting_box.crt_a, shift))]
+    # the distance between two planes of the same vector is always the third vector (in a parallelepiped at least)
+    face_4 = [-1 * i for i in agv.get_plane(cutting_box.crt_a, cutting_box.crt_b, vt_p=cutting_box.crt_c + shift)]
+    face_5 = [-1 * i for i in agv.get_plane(cutting_box.crt_c, cutting_box.crt_a, vt_p=cutting_box.crt_b + shift)]
+    face_6 = [-1 * i for i in agv.get_plane(cutting_box.crt_b, cutting_box.crt_c, vt_p=cutting_box.crt_a + shift)]
     main_sys.cut_shape(frame_id, inverse, face_1, face_2, face_3, face_4, face_5, face_6)
     cutting_box.box_cart2lmp()
 
@@ -80,16 +89,16 @@ if __name__ == "__main__":
     lmpdat_help = "lammps' data-file which defines the cutting shape."
     dcd_help = "lammps' dcd-file which defines the cutting shape."
     f_help = "Frame index of the desired frame"
-    rep_help = "replicate cell in cell vector a, b and c direction this many times."
+    rep_help = "replicate cell in cell vector a, b and c direction this many times. Replication takes place before shifting the coordinates."
     shft_help = "Translate the coordinates of the (replicated) system to cut from by this translational vector."
-    cut_by_shape_help = "Cut the exact shape of the given system. If a lmpdat_cut is given, atomic radii will be considered while cutting."
+    cut_by_shape_help = "Cut the exact shape of the given system. If a lmpdat_cutshape is given, atomic radii will be considered while cutting."
 
-    parser.add_argument("-lmpdat_cut", default=None, metavar="*.lmpdat", help=lmpdat_help)
-    parser.add_argument("-dcd_cut", default=None, metavar="*.dcd", help=dcd_help)
-    parser.add_argument("-f_cut", default=-1, type=int, metavar="0", help=f_help)
-    parser.add_argument("-rep_cut", default=None, nargs=6, type=int, metavar=12, help=rep_help)
-    parser.add_argument("-shft_cut", nargs=3, type=float, default=(0.0, 0.0, 0.0), metavar=("sx, sy, sz"), help=shft_help)
-    parser.add_argument("-cut_by_shape", default=False, action="store_true", help=cut_by_shape_help)
+    parser.add_argument("-lmpdat_cutshape", default=None, metavar="*.lmpdat", help=lmpdat_help)
+    parser.add_argument("-dcd_cutshape", default=None, metavar="*.dcd", help=dcd_help)
+    parser.add_argument("-f_cutshape", default=-1, type=int, metavar="0", help=f_help)
+    parser.add_argument("-rep_cutshape", default=None, nargs=6, type=int, metavar=12, help=rep_help)
+    parser.add_argument("-shft_cutshape", default=None, nargs=3, type=float, metavar=("sx, sy, sz"), help=shft_help)
+    parser.add_argument("-exact_cutshape", default=False, action="store_true", help=cut_by_shape_help)
 
     #-------------------------------------------------------------------------------
     # DEFINE CUTTING SHAPE BY THE GIVEN BOX
@@ -109,14 +118,15 @@ if __name__ == "__main__":
     # CUTTING OPTIONS
     #-------------------------------------------------------------------------------
     inverse_help = "Selection: box (normal), negative of box (inverted)"
-    shft_cut_result = "Shift the cut out coordinates by this vector"
+    shft_cut_result = "Shift the cut out coordinates by these 3 vectors (index 0, 1, 2: a; 3, 4, 5: b; 6, 7, 8: c"
+    enlarge_cut_help = "Enlarge the box of the cuttings shape by this vector"
     parser.add_argument("-inverse", default=False, action="store_true", help=inverse_help)
     parser.add_argument("-shft_cut_result", nargs=3, type=float, default=(0.0, 0.0, 0.0), metavar=("sx, sy, sz"), help=shft_cut_result)
+    parser.add_argument("-scale_cut", default=None, nargs=9, type=float, metavar=2.42, help=enlarge_cut_help)
 
-    parser.add_argument("-out", default="cut.lmpdat")
+    parser.add_argument("-out", default="test.lmpdat")
 
     args = parser.parse_args()
-
 
     #===============================================================================
     # PREPARE THE MAIN SYSTEM
@@ -124,7 +134,7 @@ if __name__ == "__main__":
     sys_cutfrom = ag_lammps.read_lmpdat(args.lmpdat, dcd=args.dcd, frame_idx_start=args.f, frame_idx_stop=args.f)
 
     # replicate cell if desired
-    if args.rep_cut is not None:
+    if args.rep_cutshape is not None:
         sys_cutfrom.replicate_cell(n_start=args.rep[0], n_stop=args.rep[1], direction="a", frame_id=-1, adjust_box=True)
         sys_cutfrom.replicate_cell(n_start=args.rep[2], n_stop=args.rep[3], direction="b", frame_id=-1, adjust_box=True)
         sys_cutfrom.replicate_cell(n_start=args.rep[4], n_stop=args.rep[5], direction="c", frame_id=-1, adjust_box=True)
@@ -134,34 +144,34 @@ if __name__ == "__main__":
     # shift sys_cutfrom by given vector
     if args.shft is not None:
         args.shft = np.array(args.shft)
-        M_shft_cut = cgt.translation_matrix(args.shft)
+        M_shft = cgt.translation_matrix(args.shft)
         atm_idxs = range(len(sys_cutfrom.atoms))
-        sys_cutfrom.mm_atm_coords(-1, M_shft_cut, False, *atm_idxs)
-        del (atm_idxs, M_shft_cut)
+        sys_cutfrom.mm_atm_coords(-1, M_shft, False, *atm_idxs)
+        del (atm_idxs, M_shft)
 
     #===============================================================================
     # PREPARE THE CUTTING SHAPE SYSTEM
     #===============================================================================
-    if args.lmpdat_cut is not None or args.dcd_cut is not None:
-        sys_cutshape = ag_lammps.read_lmpdat(args.lmpdat_cut, dcd=args.dcd_cut, frame_idx_start=args.f_cut, frame_idx_stop=args.f_cut)
+    if args.lmpdat_cutshape is not None or args.dcd_cutshape is not None:
+        sys_cutshape = ag_lammps.read_lmpdat(args.lmpdat_cutshape, dcd=args.dcd_cutshape, frame_idx_start=args.f_cutshape, frame_idx_stop=args.f_cutshape)
 
         # replicate cell if desired
-        if args.rep_cut is not None:
-            sys_cutshape.replicate_cell(n_start=args.rep_cut[0], n_stop=args.rep_cut[1], direction="a", frame_id=-1, adjust_box=True)
-            sys_cutshape.replicate_cell(n_start=args.rep_cut[2], n_stop=args.rep_cut[3], direction="b", frame_id=-1, adjust_box=True)
-            sys_cutshape.replicate_cell(n_start=args.rep_cut[4], n_stop=args.rep_cut[5], direction="c", frame_id=-1, adjust_box=True)
+        if args.rep_cutshape is not None:
+            sys_cutshape.replicate_cell(n_start=args.rep_cutshape[0], n_stop=args.rep_cutshape[1], direction="a", frame_id=-1, adjust_box=True)
+            sys_cutshape.replicate_cell(n_start=args.rep_cutshape[2], n_stop=args.rep_cutshape[3], direction="b", frame_id=-1, adjust_box=True)
+            sys_cutshape.replicate_cell(n_start=args.rep_cutshape[4], n_stop=args.rep_cutshape[5], direction="c", frame_id=-1, adjust_box=True)
 
-        # shift sys_cutshape by given vector
-        if args.shft_cut is not None:
-            args.shft_cut = np.array(args.shft_cut)
-            M_shft_cut = cgt.translation_matrix(args.shft_cut)
-            atm_idxs = range(len(sys_cutshape.atoms))
+        # shift the coordinates (not the box!) of sys_cutshape by given vector
+        # (for cutting the exact shape of sys_cutshape)
+        if args.shft_cutshape is not None:
+            args.shft_cutshape = np.array(args.shft_cutshape)
+            M_shft_cut = cgt.translation_matrix(args.shft_cutshape)
+            atm_idxs = range(len(sys_cutshape.ts_coords[-1]))
             sys_cutshape.mm_atm_coords(-1, M_shft_cut, False, *atm_idxs)
             del (atm_idxs, M_shft_cut)
 
     else:
         sys_cutshape = None
-
 
     #===============================================================================
     # PREPARE BOX WHICH SOLELY DEFINES THE SHAPE
@@ -171,7 +181,7 @@ if __name__ == "__main__":
 
         if args.boxtype == "lammps":
             box = mdb.Box(boxtype=args.boxtype, lmp_xlo=args.box[0], lmp_xhi=args.box[1], lmp_ylo=args.box[2], lmp_yhi=args.box[3], lmp_zlo=args.box[4], lmp_zhi=args.box[5], lmp_xy=args.box[6], lmp_xz=args.box[7], lmp_yz=args.box[8])
-            box.box_lmp2cart()  # change box vectors to lattice
+            #box.box_lmp2cart()  # change box vectors to lattice
         elif args.boxtype == "lattice":
             box = mdb.Box(boxtype=args.boxtype, ltc_a=args.box[0], ltc_b=args.box[1], ltc_c=args.box[2], ltc_alpha=math.radians(args.box[3]), ltc_beta=math.radians(args.box[4]), ltc_gamma=math.radians(args.box[5]))
             box.box_lat2cart()
@@ -180,14 +190,23 @@ if __name__ == "__main__":
     else:
         box = None
 
-    #===============================================================================
-    # CUT THE BOX
-    #===============================================================================
+    # equalize box variable
     if sys_cutshape is not None:
         box = sys_cutshape.ts_boxes[-1]
 
-    # cut the shape
-    cutting(sys_cutfrom, box, args.inverse, -1)
+    if box.boxtype == "lammps":
+        box.box_lmp2cart()
+
+    #===============================================================================
+    # CUT THE BOX
+    #===============================================================================
+    # change box by vector
+    if args.scale_cut is not None:
+        box.crt_a += np.array(args.scale_cut[0:3])
+        box.crt_b += np.array(args.scale_cut[3:6])
+        box.crt_c += np.array(args.scale_cut[6:9])
+
+    cutting(sys_cutfrom, box, args.inverse, -1, shift=args.shft_cutshape)
 
     #===============================================================================
     # CUT THE BOX
@@ -196,8 +215,15 @@ if __name__ == "__main__":
     if args.inverse is True:
         sys_cutfrom.ts_boxes[-1] = box
 
-    pdb.set_trace()
+    #pdb.set_trace()
     sys_cutfrom.refresh()
     sys_cutfrom.change_indices(incr=1, mode="increase")
+
+    if args.shft_cut_result is not None:
+        M_shft_cut = cgt.translation_matrix(args.shft_cut_result)
+        atm_idxs = range(len(sys_cutfrom.ts_coords[-1]))
+        sys_cutfrom.mm_atm_coords(-1, M_shft_cut, False, *atm_idxs)
+        del (atm_idxs, M_shft_cut)
+
     print("Writing lammps data file...")
     sys_cutfrom.write_lmpdat(args.out, frame_id=-1, title="Cut box", cgcmm=True)
