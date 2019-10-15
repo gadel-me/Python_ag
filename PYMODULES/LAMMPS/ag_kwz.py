79fa9e5e21d5ef3af583b707f433cf53e1797ef1
gadel-me <andreas.gadelmeier@gmx.net>
2019-10-15 23:59:11 +0200

    comm_style was not set after pair_coeffs

--- PYMODULES/LAMMPS/ag_kwz.py
+++ PYMODULES/LAMMPS/ag_kwz.py
@@ -196,16 +196,15 @@ def md_simulation(lmpcuts, group, style, ensemble, keyword_min=None, keyword=Non
     lmp.file(lmpcuts.settings_file)
     lmpcuts.load_system(lmp)
 
-    # distribute the available cores if we have lots of vacuum in our box
-    # communication overhead? slower than before!
-    lmp.command("comm_style tiled")
-    lmp.command("balance 1.0 rcb")
-
     lmpcuts.thermo(lmp)
     lmpcuts.dump(lmp, unwrap=unwrap_dcd)
 
     if lmpcuts.pc_file is not None:
         lmp.file(lmpcuts.pc_file)
+
+    # distribute the available cores; option must be set after loading pair coeffs!
+    lmp.command("comm_style tiled")
+    lmp.command("balance 1.0 rcb")
 
     # pre-minimize system before running the actual simulation
     if lmpcuts.input_lmprst is None or os.path.isfile(lmpcuts.input_lmprst):
@@ -544,10 +543,6 @@ def quench(lmpcuts, lmpdat_main, runs=20, split=None):
     lmp.file(lmpcuts.settings_file)
     lmpcuts.load_system(lmp)
 
-    # distribute the available cores if we have lots of vacuum in our box
-    lmp.command("comm_style tiled")
-    lmp.command("balance 1.0 rcb")
-
     #lmp.command("velocity all create {} {} mom yes rot yes dist gaussian".format(lmpcuts.tstart, np.random.randint(29847587)))
     lmp.command("fix ic_prevention all momentum 100 linear 1 1 1 angular rescale")
     lmpcuts.dump(lmp, unwrap=True)
@@ -555,6 +550,10 @@ def quench(lmpcuts, lmpdat_main, runs=20, split=None):
 
     if lmpcuts.pc_file is not None:
         lmp.file(lmpcuts.pc_file)
+
+    # distribute the available cores if we have lots of vacuum in our box
+    lmp.command("comm_style tiled")
+    lmp.command("balance 1.0 rcb")
 
     # define the atoms that may move during the simulation
     lmp.command("group grp_add_sys id > {}".format(natoms_main_sys))
@@ -1004,15 +1003,15 @@ def _anneal(lmpcuts, pe_atm_idxs, ensemble, group="all", keyword="iso"):
     lmp.file(lmpcuts.settings_file)
     lmpcuts.load_system(lmp)
 
-    # distribute the available cores if we have lots of vacuum in our box
-    lmp.command("comm_style tiled")
-    lmp.command("balance 1.0 rcb")
-
     lmp.command("fix ic_prevention all momentum 100 linear 1 1 1 angular rescale")
     lmpcuts.dump(lmp, unwrap=True)
 
     if lmpcuts.pc_file is not None:
         lmp.file(lmpcuts.pc_file)
+
+    # distribute the available cores if we have lots of vacuum in our box
+    lmp.command("comm_style tiled")
+    lmp.command("balance 1.0 rcb")
 
     # compute potential energy of the solvate (pair, bond, ...)
     # in order to compute the pe of the solvent, the solvent atom-ids must be
